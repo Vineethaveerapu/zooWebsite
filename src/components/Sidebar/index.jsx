fd85a53e186db31bb4bccb82878cff75ce6e86e5
vineetha <vineethaveerapu4@gmail.com>
2025-04-15 14:23:38 +0200

    sidebar title added

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,14 +1,19 @@
 import styles from "./Sidebar.module.css";
 
-const Sidebar = ({ animals, handleSidebarClick, activeItem }) => {
+const Sidebar = ({ animals, handleSidebarClick, activeItem, title }) => {
   return (
     <aside className={styles.sidebar}>
+      <div className={styles.sidebarHeader}>
+        <h2>{title}</h2>
+      </div>
       <div className={styles.sidebarContent}>
         {animals.map((animal) => (
           <div
             key={animal.name}
             onClick={() => handleSidebarClick(animal)}
-            className={activeItem === animal ? styles.active : ""}
+            className={`${styles.sidebarItem} ${
+              activeItem === animal ? styles.active : ""
+            }`}
           >
             {animal.name}
           </div>
