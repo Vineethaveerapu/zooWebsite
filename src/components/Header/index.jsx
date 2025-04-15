45ec3d37f147cf76e21009b413da140f55e57100
vineetha <vineethaveerapu4@gmail.com>
2025-04-15 20:25:59 +0200

    navigation component are added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,55 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "/src/assets/logo.png";
-import { pages } from "/src/data/pages.js";
+
 import styles from "./Header.module.css";
 import { List, X } from "@phosphor-icons/react";
-import { useState, useEffect } from "react";
+import Navigation from "../Navigation";
 
 const Header = () => {
   // get current page from router
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Close mobile menu when location changes
-  useEffect(() => {
-    setIsOpen(false);
-  }, [location]);
-
-  const menuButtonClick = () => {
-    setIsOpen(isOpen ? false : true);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
 
   return (
     <header className={styles.header}>
       <Link to="/" aria-label="Home">
         <img src={logo} alt="Zoo Logo" width={90} height={90} />
       </Link>
-      <nav
-        className={styles.nav + (isOpen ? " " + styles.open : "")}
-        aria-label="Main Navigation"
-      >
-        {pages.map((page) => (
-          <Link
-            to={page.slug}
-            key={page.slug}
-            className={location.pathname === page.slug ? styles.active : ""}
-            aria-current={location.pathname === page.slug ? "page" : undefined}
-            onClick={closeMenu}
-          >
-            {page.name}
-          </Link>
-        ))}
-      </nav>
-      <button
-        onClick={menuButtonClick}
-        className={styles.mobileMenuButton + (isOpen ? " " + styles.open : "")}
-      >
-        {isOpen ? <X size={32} /> : <List size={32} />}
-      </button>
+      <Navigation />
     </header>
   );
 };
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,52 @@
+import { pages } from "/src/data/pages.js";
+import { Link, useLocation } from "react-router-dom";
+import styles from "../Header/Header.module.css";
+import { useState, useEffect } from "react";
+import { List, X } from "@phosphor-icons/react";
+
+const Navigation = () => {
+  const location = useLocation();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  // Close mobile menu when location changes
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
+  const menuButtonClick = () => {
+    setIsOpen(isOpen ? false : true);
+  };
+
+  return (
+    <>
+      <nav
+        className={styles.nav + (isOpen ? " " + styles.open : "")}
+        aria-label="Main Navigation"
+      >
+        {pages.map((page) => (
+          <Link
+            to={page.slug}
+            key={page.slug}
+            className={location.pathname === page.slug ? styles.active : ""}
+            aria-current={location.pathname === page.slug ? "page" : undefined}
+            onClick={closeMenu}
+          >
+            {page.name}
+          </Link>
+        ))}
+      </nav>
+      <button
+        onClick={menuButtonClick}
+        className={styles.mobileMenuButton + (isOpen ? " " + styles.open : "")}
+      >
+        {isOpen ? <X size={32} /> : <List size={32} />}
+      </button>
+    </>
+  );
+};
+
+export default Navigation;
