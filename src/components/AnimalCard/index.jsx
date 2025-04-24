ba88983b64396665015dce00000117539203801e
vineetha <vineethaveerapu4@gmail.com>
2025-04-24 23:59:47 +0200

    adding dialog

--- src/components/AnimalCard/index.jsx
+++ src/components/AnimalCard/index.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "../FeaturedContent/FeaturedContent.module.css";
+import { XSquare } from "@phosphor-icons/react";
+import animalCardStyles from "./AnimalCard.module.css";
 
 const AnimalCard = ({ pageName, activeItem, handleSidebarClick }) => {
   const [showReadMore, setShowReadMore] = useState(false);
-
+  const modalRef = useRef(null);
   // Reset showReadMore when activeItem changes
   useEffect(() => {
     setShowReadMore(false);
@@ -34,48 +36,87 @@ const AnimalCard = ({ pageName, activeItem, handleSidebarClick }) => {
     setShowReadMore(!showReadMore);
   };
 
-  return (
-    <section className={styles.featuredContentContainer}>
-      <div className={styles.featuredContent}>
-        <h2>{name}</h2>
-        <img src={image} alt={name} height={300} width={300} />
-        {showReadMore ? (
-          <div>
-            <p>Name: {name}</p>
-            <p>Weight: {weight}</p>
-            <p>Length: {length}</p>
-            <p>Food: {food}</p>
-            <p>Lifespan: {lifespan}</p>
-            <p>Found: {found}</p>
-            <p>Group: {group}</p>
-            <p>Description: {description}</p>
-          </div>
-        ) : (
-          <p>{shortDescription}...</p>
-        )}
+  const openModal = () => {
+    modalRef.current.showModal();
+  };
 
-        {isHomePage ? (
-          <>
-            <Link
-              to={{
-                pathname: `/${groupSlug}`,
-                hash: `#${activeItem.name}`
-              }}
-              onClick={() => {
-                handleSidebarClick(null);
-              }}
-              className={styles.readButton}
-            >
-              Visit {group} for more information
-            </Link>
-          </>
-        ) : (
-          <button className={styles.readButton} onClick={handleReadMore}>
-            {showReadMore ? "Read Less" : "Read More"}
-          </button>
-        )}
-      </div>
-    </section>
+  const closeModal = () => {
+    modalRef.current.close();
+  };
+
+  return (
+    <>
+      <section className={styles.featuredContentContainer}>
+        <div className={styles.featuredContent}>
+          <h2>{name}</h2>
+          <img src={image} alt={name} height={300} width={300} />
+          {showReadMore ? (
+            <div>
+              <p>Name: {name}</p>
+              <p>Weight: {weight}</p>
+              <p>Length: {length}</p>
+              <p>Food: {food}</p>
+              <p>Lifespan: {lifespan}</p>
+              <p>Found: {found}</p>
+              <p>Group: {group}</p>
+            </div>
+          ) : (
+            <p>{shortDescription}...</p>
+          )}
+
+          {isHomePage ? (
+            <>
+              <button className={styles.readButton} onClick={openModal}>
+                Read More
+              </button>
+              <Link
+                to={{
+                  pathname: `/${groupSlug}`,
+                  hash: `#${activeItem.name}`
+                }}
+                onClick={() => {
+                  handleSidebarClick(null);
+                }}
+                className={styles.readButton}
+              >
+                Visit {group} for more information
+              </Link>
+            </>
+          ) : (
+            <button className={styles.readButton} onClick={handleReadMore}>
+              {showReadMore ? "Read Less" : "Read More"}
+            </button>
+          )}
+        </div>
+      </section>
+
+      <dialog ref={modalRef}>
+        <XSquare
+          size={32}
+          onClick={closeModal}
+          className={animalCardStyles.closeButton}
+        />
+        <div>
+          <p>Name: {name}</p>
+          <p>Food: {food}</p>
+          <p>Found: {found}</p>
+          <p>Group: {group}</p>
+          <p>Description: {description}</p>
+          <Link
+            to={{
+              pathname: `/${groupSlug}`,
+              hash: `#${activeItem.name}`
+            }}
+            onClick={() => {
+              handleSidebarClick(null);
+            }}
+            className={styles.readButton}
+          >
+            Visit {group} for more information
+          </Link>
+        </div>
+      </dialog>
+    </>
   );
 };
 
